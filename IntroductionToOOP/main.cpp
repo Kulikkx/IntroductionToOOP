623bf034e03e1f8078e11af8c0ed9090c22aa080
Kulik_kx07@top-academy.ru <Kulik_kx07@top-academy.ru>
2024-05-24 21:34:41 +0300

    Method and  function distance

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -22,9 +22,64 @@ public:
 	{
 		this->y = y;
 	}
+
+	// Constructors
+
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Point( double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+
+	}
+	Point(const Point& other)
+	{
+		this-> x = other.x;
+		this-> y= other.y;
+		cout << "CopyConstructor:\t" << this<<endl;
+
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	//Methods
+	double distance(Point other)
+		//this - эта точка
+		//other - та точка
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+	void print()const
+	{
+		cout << "x= " << x << "\tY= " << y << endl;
+	}
 };
 
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
+
 //#define STRUCT_POINT
+#define DISTANCE_CHECK
+//#define CONSTRUCTOR_CHECK
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -43,8 +98,36 @@ void main()
 	Point* pA = &A; //Pointer to 'A'
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUC_POINT
+#ifdef DISTANCE_CHECK
+
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
+	
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Растояние от точки 'А' до точки 'В': " << A.distance(B) << endl;
+	cout << "Растояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
+
+#endif // DISTANCE_CHECK
+#ifdef CONSTRUCTOR_CHECK
+	Point A;//Default constructor
+	A.print();
+
+	Point B = 5;
+	B.print();
+
+	Point C(2, 3);
+	C.print();
+
+	Point D = C;
+	D.print();
+#endif // CONSTRUCTOR_CHECK
+
 }
