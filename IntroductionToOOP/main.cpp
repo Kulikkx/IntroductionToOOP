bfe6c5954c4fa0fa8a32c00cfc0fffaa293066cd
Kulik_kx07@top-academy.ru <Kulik_kx07@top-academy.ru>
2024-05-31 20:22:15 +0300

    Fraction done

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -67,6 +67,21 @@ public:
 		cout << "CopyAssignment:\t\t" << this << endl;
 		return *this;
 	}
+
+	Point& operator++() //Prefix increment
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)//Postfix increment
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
+
 	//Methods
 	double distance(const Point& other)const
 		//this - эта точка
@@ -90,10 +105,43 @@ double distance(const Point& A,const Point& B)
 	return sqrt(x_distance * x_distance + y_distance * y_distance);
 }
 
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+Point operator - (const Point& left, const Point& right)
+{
+	Point result
+	{
+		left.get_x() - right.get_x(),
+		left.get_y() - right.get_y()
+	};
+	return result;
+}
+Point operator*(const Point& left, const Point& right)
+{
+	return Point
+	(
+		left.get_x() * right.get_x(),
+		left.get_y() * right.get_y()
+		);
+}
+bool operator==(const Point& left, const Point& right)
+{
+	return (left.get_x() == right.get_x() && left.get_y() == right.get_y());
+		/*return true;
+	else return false*/
+}
+
 //#define STRUCT_POINT
 //#define DISTANCE_CHECK
 //#define CONSTRUCTOR_CHECK
-#define ASSIGNMENT_CHECK
+//#define ASSIGNMENT_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -112,6 +160,7 @@ void main()
 	Point* pA = &A; //Pointer to 'A'
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUC_POINT
+
 #ifdef DISTANCE_CHECK
 
 	Point A;
@@ -134,6 +183,7 @@ void main()
 	cout << delimiter << endl;
 
 #endif // DISTANCE_CHECK
+
 #ifdef CONSTRUCTOR_CHECK
 	Point A;//Default constructor
 	A.print();
@@ -165,5 +215,22 @@ void main()
 	C.print();
 #endif // ASSIGNMENT_CHECK
 
+#ifdef ARITHMETICAL_OPERATORS_CHECK
+	int a = 2;
+	int b = 3;
+	int c = a - b;
 
+	Point A(2, 3);
+	Point B(7, 8);
+	A.print();
+	B.print();
+	Point C = A * B;
+	C.print();
+
+	C++;
+	C.print();
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+	//cout << (2 == 3) << endl;
+	cout << (Point(2, 3) == Point(3, 3)) << endl;
 }
